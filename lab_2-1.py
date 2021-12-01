bff1fab4d9df8fc6dbe9cde87896cef20473345b
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-12-01 10:21:12 -0500

    Submit Labs 2-1 and 2-2

---
+++ lab_2-1.py
@@ -0,0 +1,9 @@
+# Author: CRS 12/01/21
+def sum(num):
+    total = 0
+    for value in range(int(num)+1):
+        total += value
+
+
+number = input("Input a number.")
+sum(sum(number))
---
+++ lab_2-2.py
@@ -0,0 +1,7 @@
+# Author: CRS 12/01/21
+def invites(lst):
+    for name in lst:
+        print("Hi {0}, you're invited to my party.".format(name))
+
+
+invites(["Michael", "Jake", "Steve"])
